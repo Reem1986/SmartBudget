e9dcb25f147c7ecdee48e3499f5c636a3ae00738
Reem1986 <1292112921>
2014-09-23 00:31:40 +1000

    create tabs and layouts

--- gen/com/reem/smartbudget/R.java
+++ gen/com/reem/smartbudget/R.java
@@ -25,13 +25,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080007;
+        public static final int button1=0x7f080004;
         public static final int buttonSubmit=0x7f080000;
+        public static final int checkBox1=0x7f080003;
         public static final int editTextPassword=0x7f080001;
+        public static final int pager=0x7f080006;
+        public static final int seekBar1=0x7f080005;
         public static final int textView1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int fragment_budget=0x7f030001;
+        public static final int fragment_reminder=0x7f030002;
+        public static final int fragment_reports=0x7f030003;
+        public static final int fragment_settings=0x7f030004;
+        public static final int fragment_shoppinglist=0x7f030005;
+        public static final int fragment_transfer=0x7f030006;
+        public static final int home=0x7f030007;
     }
     public static final class menu {
         public static final int login=0x7f070000;
