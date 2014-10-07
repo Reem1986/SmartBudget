cd2b1c2244a54a5e7a9defd4f10a106cb6c3efb0
Reem1986 <1292112921>
2014-10-07 12:01:33 +1000

    Add button on budget page
    Fragment Budget java
    Fragment Budget Add java
    Fragment Budget xml
    Fragment Budget Add xml

---
+++ src/com/reem/smartbudget/FragmentBudgetAdd.java
@@ -0,0 +1,16 @@
+package com.reem.smartbudget;
+
+import com.reem.smartbudget.R;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class FragmentBudgetAdd extends FragmentActivity {
+	  @Override
+	    protected void onCreate(Bundle savedInstanceState)
+	    {
+	        super.onCreate(savedInstanceState);
+
+	        setContentView(R.layout.fragment_budget_add);
+	    }
+}
